0ea9f06a9437b1aa2dbe59c3807f0816bc5c87f9
Ben Pollock <B.O.Pollock@gmail.com>
2013-03-09 22:35:24 -0500

    Initial Commit of Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bpollock::Application.config.secret_token = '629751968f94a2172847fd0ec9244619a9e9a53ba1d1828565129f1531dad5a530befbf679401cafbb7733991c3139dd940e76b9a05ab2b48439419dee7cfc0b'
